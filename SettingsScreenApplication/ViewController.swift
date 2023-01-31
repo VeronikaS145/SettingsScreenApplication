b57d42e7c2d422f5d32160a1df825475f30a6f8f
VeronikaSemenkova <nika.siemienkova145@gmail.com>
2023-02-01 02:36:52 +0300

    Initial Commit

---
+++ SettingsScreenApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsScreenApplication
+//
+//  Created by Nika Semenkova on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
