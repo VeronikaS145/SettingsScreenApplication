e580c35039da6b6f5024bfa5e123b451a54a3e15
VeronikaSemenkova <nika.siemienkova145@gmail.com>
2023-02-07 07:28:23 +0300

    Delete textField and add searchBar.

--- SettingsScreenApplication/Sources/MainModule/ViewController.swift
+++ SettingsScreenApplication/Sources/MainModule/ViewController.swift
@@ -14,14 +14,13 @@ class ViewController: UIViewController {
     
     // MARK: - UI Elements
     
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.textColor = .black
-        textField.textAlignment = .left
-        textField.placeholder = "Поиск"
-        textField.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        textField.layer.cornerRadius = 5
-        return textField
+    private lazy var searchBar: UISearchController = {
+        let search = UISearchController()
+        search.searchBar.placeholder = "Поиск"
+        search.obscuresBackgroundDuringPresentation = true
+        search.searchBar.autocapitalizationType = .words
+        search.searchBar.layer.cornerRadius = 5
+        return search
     }()
     
     private lazy var tableView: UITableView = {
@@ -40,9 +39,6 @@ class ViewController: UIViewController {
         setupView()
         setupHierarchy()
         setupLayout()
-        
-        hideKeyboard()
-        setIconTextField()
     }
     
     // MARK: - Setups
@@ -50,54 +46,25 @@ class ViewController: UIViewController {
         title = "Настройки"
         view.backgroundColor  = #colorLiteral(red: 0.9499780536, green: 0.9505755305, blue: 0.9688754678, alpha: 1)
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchBar
         model = Cell.cells
-        hideKeyboard()
-        setIconTextField()
     }
     
     private func setupHierarchy() {
-        view.addSubview(textField)
         view.addSubview(tableView)
     }
     
     private func setupLayout() {
-        textField.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.centerX.equalTo(view)
-            make.width.equalTo(350)
-            make.height.equalTo(30)
-        }
-        
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(textField.snp.bottom).offset(1)
+            make.top.equalToSuperview()
             make.bottom.left.right.equalTo(view)
         }
-    }
-    
-    // MARK: - Private functions
-    
-    private func setIconTextField() {
-        if let textFieldIcon = UIImage(systemName: "magnifyingglass") {
-            textField.setLeftIcon(textFieldIcon)
-        }
-    }
-    
-    // MARK: - Actions
-    
-    // Hide keyboard using a tap gesture
-    @objc func hideKeyboard() {
-        let tapGesture = UITapGestureRecognizer(target: self,
-                                                action: #selector(hideKeyboard))
-        tapGesture.cancelsTouchesInView = false
-        self.view.addGestureRecognizer(tapGesture)
-        self.view.endEditing(true)
     }
 }
 
 // MARK: - Extensions
 
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if model?[indexPath.section] == model?[0] { return 80 } else { return 40 }
     }
